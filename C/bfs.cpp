89278e13848e655049b35bcc22c15cbbac58bf2c
Anubhav Kesari <kesarianubhav@gmail.com>
2017-12-30 19:10:53 +0530

    Create bfs.cpp

---
+++ C/bfs.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<algorithm>
+#include<queue>
+#include<bits/stdc++.h>
+#define FI(i,a,b) for(int i=a;i<b;i++)
+#define output(x) printf("x")
+#define pb push_back
+using namespace std;
+int graph[1000][1000];
+bool check[1000006];
+bool visited[100006];
+
+vector<int> output;
+void bfs( int n , vector<int> v[] , int start  )
+{
+	visited[start] = true ;
+	queue<int> q;
+	q.push(start);
+	while(!q.empty())
+	{
+		int p = q.front();
+		q.pop();
+		output.pb(p);
+		for(std::vector<int>::iterator it = v[p].begin();it!=v[p].end();++it)
+			if(visited[(*it)]==false) 
+			{
+				visited[*it]=true;
+				q.push(*it);
+			}
+	}
+}
+
+
+
+void graph_printer(int v , vector<int> ve[])
+{
+	FI(i,0,v)
+	{
+		std::vector<int> :: iterator it ;
+		for( it = ve[i].begin();it!=ve[i].end();++it)
+			cout<<(*it)<<"\t";
+		cout<<"\n";
+	}
+}
+
+int main()
+{
+	int v;
+	cin >> v;
+	vector<int> adjacency[v];
+	int a;
+	FI(i,0,v)
+	{
+		FI(j,0,v)
+		{ 
+		if(i==j) continue;
+		cout<<"b/w "<<i<<"and"<<j<<"\n"; 
+		cin >>a; 
+		if(a==1) 
+				adjacency[i].pb(j); 
+		}
+	}	
+	graph_printer(v,adjacency);
+	bfs(v,adjacency,0);
+	std::vector<int> :: iterator it ;
+	cout<<"\n";
+	for( it = output.begin();it!=output.end();++it)
+		cout<<(*it)<<"\t";
+	
+}
+
+ 
